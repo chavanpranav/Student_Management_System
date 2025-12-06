22226775f16cc4e93af34d77ae8d72c529feb4f1
chavanpranav <chavanvarad514@gmail.com>
2025-12-06 23:30:44 +0530

    write a code for edit/update student

--- Backend/routes/studentRoutes.js
+++ Backend/routes/studentRoutes.js
@@ -43,8 +43,46 @@ router.get("/:id", (req, res) => {
 });
 
 //edit student 
-router.patch("")
+router.get("/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let q3 = `SELECT * FROM students WHERE id = ${id}`;
 
+    try {
+
+        connection.query(q3, (err, result) => {
+            if(err) throw err;
+            let student = result[0];
+            res.render("edit", {student});
+        });
+    }catch(err) {
+        console.log("error");
+    }
+});
+
+
+//update route
+router.patch("/:id", (req, res) => {
+    let {id} = req.params;
+    let {name : newName, phone : newPhone} = req.body;
+    let q4 = `SELECT * FROM students WHERE id = ${id}`;
+
+    try {
+
+        connection.query(q4, (err, result) => {
+            if(err) throw err;
+            let student = result[0];
+
+            let q5 = `UPDATE students set name = ? , phone = ? WHERE id = ?`;
+            const values = [newName, newPhone, id];
+            connection.query(q5, values, (err, result) => {
+                    if(err) throw err;
+                    res.redirect("/students");
+                });
+        });
+    }catch(err) {
+        console.log("error");
+    }
+});
 
 
 
