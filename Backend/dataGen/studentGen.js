4e0484dec1382c0ec18b901d7599bc625b4eda69
chavanpranav <chavanvarad514@gmail.com>
2025-12-18 10:30:51 +0530

    update README.md

--- Backend/dataGen/studentGen.js
+++ Backend/dataGen/studentGen.js
@@ -6,10 +6,9 @@ const {faker} = require("@faker-js/faker");
 const connection = require("../sqlNodeConnection/db");
 
 const courses = ["C++", "Java", "Python", "AI", "ML", "WebDev", "DevOps", "Cloud"];
-let idcnt = 1;
+
 let createUser = ()=> {
     return [
-        idcnt++,
         faker.person.fullName(),
         faker.internet.email(),
          faker.number.int({ min: 18, max: 25 }), 
@@ -18,13 +17,14 @@ let createUser = ()=> {
     ];
 };
 
+
 const data = [];
 
-for(let i = 1; i < 200; i++) {
+for(let i = 0; i < 100; i++) {
     data.push(createUser());
 }
 
-let q = `INSERT INTO STUDENTS (id, name, email, age, course, phone) VALUES ?`;
+let q = `INSERT INTO STUDENTS (name, email, age, course, phone) VALUES ?`;
 
 try{
     connection.query(q, [data], (err, result) => {
